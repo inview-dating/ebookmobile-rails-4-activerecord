04eab8d996a9a1df1db4a9a8bc23b1a4aa3a3d71
Dana Scheider <dana.scheider@gmail.com>
2016-07-17 11:34:10 -0700

    Generate Rails 4 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EbookmobileRails4Activerecord::Application.config.secret_key_base = 'bc7ba8eb418d8fbf63f6766cd0550dff641619e3c04ecbd894a88b434f37cbffe46927544c97357dcf4c6e16ac07b292c13c1f37c38d3ea0c6c4505f732aa734'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EbookmobileRails4Activerecord::Application.config.session_store :cookie_store, key: '_ebookmobile-rails-4-activerecord_session'
